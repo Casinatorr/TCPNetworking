b29ac0d8b493262d6f76cb5049abcc0f9e8099e6
Casinator <caspar@schneider-a.com>
2021-01-05 22:05:53 +0100

    Test Release

--- TCPNetworkingServer/Program.cs
+++ TCPNetworkingServer/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetworkingTesting
+namespace TCPNetworkingServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Server.Start(50, 26950);
-            Console.ReadKey();
         }
     }
 }
--- TCPNetworkingServer/Properties/AssemblyInfo.cs
+++ TCPNetworkingServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetworkingTesting")]
+[assembly: AssemblyTitle("TCPNetworkingServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetworkingTesting")]
+[assembly: AssemblyProduct("TCPNetworkingServer")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d12d5289-7747-400c-ae84-ae030ca379da")]
+[assembly: Guid("a23dc89f-649f-4540-a139-60ff6059de99")]
 
 // Version information for an assembly consists of the following four values:
 //
